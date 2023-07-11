e6301feea446b419b38fc095bc40aaddd426ea8d
TheCodister <quang.nguyen26012907@hcmut.edu.vn>
2023-07-11 09:48:16 +0700

    Add about me info and animation

--- src/Components/MyInfo/index.jsx
+++ src/Components/MyInfo/index.jsx
@@ -3,6 +3,7 @@ const MyInfo = () => {
     const content = [
         {title: 'Name: ', contents:'Nguyen Ngoc Quang.'},
         {title: 'Age: ', contents:'21.'},
+        {title: 'Mail: ', contents:'quangcuber002@gmail.com'},
         {title: 'Occupation: ', contents:'University student.'},
         {title: 'School: ', contents:'Ho Chi Minh University of Technology.'},
         {title: 'Major: ', contents:'Computer Science.'},
@@ -13,10 +14,12 @@ const MyInfo = () => {
         <div className="info">
             <img src="/Image/Profilepic.jpg" alt="propic" className="info-profile"></img>
             <div className="info-content">
+                <h1 className="info-content-title">Summary</h1>
                 {
                    content.map((content) => (
                         <div className="info-content-text">
-                            <h1>{content.title}{content.contents}</h1>
+                            <h1>{content.title}</h1>
+                            <h2>{content.contents}</h2>
                         </div>
                    )) 
                 }
